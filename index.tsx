2b2f763f21e12a592685caac06c1358f63fbf3ad
Daram Guraiah <143159095+DaramGuru95@users.noreply.github.com>
2024-02-22 21:08:03 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+// if (process.env.NODE_ENV == "production") {
+//   document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+//   });
+//   const ctrlShiftKey = (e: any, keyCode: any) => {
+//     return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+//   };
+
+//   document.onkeydown = (e: any) => {
+//     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//     if (
+//       e.keyCode === 123 ||
+//       ctrlShiftKey(e, "I") ||
+//       ctrlShiftKey(e, "J") ||
+//       ctrlShiftKey(e, "C") ||
+//       (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+//     )
+//       return false;
+//   };
+// }
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
